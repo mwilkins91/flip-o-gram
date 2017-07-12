6189c06f4da19a3dde419c329e9b7557398b7e25
romern <nicolas.romer@uberflip.com>
2017-07-12 12:13:45 -0400

    radar: make into a jquery plugin

--- radarchart.js
+++ radarchart.js
@@ -1,122 +1,133 @@
 
-var flipogram = function (data) {
 
-  //$ = jQuery;
+(function ( $ ) {
 
-  var config = {
-    node: '#flipogram'
-  };
+  // 'this' is the jquery object that this is being called on
 
-  // Customizable options
-  var numberOfRings = 5; //the number of rings
-  var marginAroundChart = 0.7;
-  var colors = ["#87ceeb", "#fcd72a", "#ce0058", "#70c176"];
+  $.fn.flipogram = function ( options ) {
 
-  //An array from 0 to the number of spokes
-  var spokes = [];
-  for (var i = 0; i < data.labels.length; i++) {
-    spokes.push(i);
-  }
+    var settings = $.extend({
+      // These are the defaults.
+      data: {
+        values: [],
+        labels: [1,2,3,4,5,6,7,8,9,10,11,12],
+        categories: []
+      },
+      colors: ["#87ceeb", "#fcd72a", "#ce0058", "#70c176"],
+      margin: 0.7,
+      ringsCount: 5,
+      width: 825,
+      height: 600
+    }, options );
 
-  // The values for each ring (i.e. level 1 == 0.2)
-  // the `1` is to create a background for styling
-  var ringRadii = [1];
-  for (var i = 0; i < numberOfRings; i++) {
-    ringRadii.push((i + 1) / numberOfRings)
-  }
+    this.css({
+      width:  settings.width + 'px',
+      height: settings.height + 'px'
+    });
 
-  // Set up measurements for drawing chart
-  var $radarDiv = $(config.node);
-  var absoluteMaxDiameter = Math.min($radarDiv.width() * 2, $radarDiv.height() * 2);
-  var maximumChartDiameter = absoluteMaxDiameter - (absoluteMaxDiameter * marginAroundChart);
-  var radius = (maximumChartDiameter / 2);
-  var maximumTextDiameter = maximumChartDiameter + (maximumChartDiameter * 0.1);
-  var distanceBetweenSpokes = 2 * Math.PI / spokes.length;
+    // Set up container SVG
+    var d3RadarSvg = d3.select(this.selector).append('svg').attr('id', 'radarSvg');
 
+    function initCenteredSvg(className) {
+      var result = d3RadarSvg
+        .append('svg')
+        .attr('class', className)
+        .attr('x', '50%')
+        .attr('y', '50%');
+      return result
+    }
 
-  // Set up container SVG
-  var d3RadarSvg = d3.select(config.node).append('svg').attr('id', 'radarSvg');
+    //An array from 0 to the number of spokes
+    var spokes = [];
+    for (var i = 0; i < settings.data.labels.length; i++) {
+      spokes.push(i);
+    }
 
-  function initCenteredSvg(className) {
-    var result = d3RadarSvg
-      .append('svg')
-      .attr('class', className)
-      .attr('x', '50%')
-      .attr('y', '50%');
-    return result
-  }
+    // The values for each ring (i.e. level 1 == 0.2)
+    // the `1` is to create a background for styling
+    var ringRadii = [1];
+    for (var i = 0; i < settings.ringsCount; i++) {
+      ringRadii.push((i + 1) / settings.ringsCount)
+    }
 
-  var drawWebbing = function () {
-    //  Initialize container for webbing
-    var webbingContainer = initCenteredSvg('radarWebbingContainer');
+    // Set up measurements for drawing chart
+    var diameterLimit = Math.min(this.width() * 2, this.height() * 2);
+    var diameter = diameterLimit - (diameterLimit * settings.margin);
+    var radius = (diameter / 2);
+    var textRadius = (diameter + (diameter * 0.1)) / 2;
+    var distanceBetweenSpokes = 2 * Math.PI / spokes.length;
 
-    // draw the webbing
-    webbingContainer
-      .append("svg:g")
-      .attr('class', 'radarWebbing')
-      .selectAll(".radarWeb")
-      .data(ringRadii)
-      .enter()
+    var drawWebbing = function () {
+      //  Initialize container for webbing
+      var webbingContainer = initCenteredSvg('radarWebbingContainer');
+
+      // draw the webbing
+      webbingContainer
+        .append("svg:g")
+        .attr('class', 'radarWebbing')
+        .selectAll(".radarWeb")
+        .data(ringRadii)
+        .enter()
         .append("svg:polygon")
         .attr('class', function (_, i) {
           return 'radarWeb' + i + ' radarWeb';
         })
         //create a spoke-ring for each fraction in ringRadii
         .attr("points",
-          function (value) {
+          function (ringRadius) {
             var points = "";
             $.each(spokes, function(i, spoke){
-              var x = value * radius * Math.cos(spoke * distanceBetweenSpokes);
-              var y = value * radius * Math.sin(spoke * distanceBetweenSpokes);
+              var x = ringRadius * radius * Math.cos(spoke * distanceBetweenSpokes);
+              var y = ringRadius * radius * Math.sin(spoke * distanceBetweenSpokes);
               points += x + "," + y + " ";
             });
             return points;
           }
         )
-  };
+    };
 
-  var drawSpokes = function () {
-    // Initialize container for spokes, to center
-    var spokeContainer = initCenteredSvg('radarSpokeContainer');
+    var drawSpokes = function () {
+      // Initialize container for spokes, to center
+      var spokeContainer = initCenteredSvg('radarSpokeContainer');
 
-    // build spokes
-    spokeContainer
-      .append('svg:g')
-      .attr('class', 'radarSpoke')
-      .selectAll('.spoke')
-      .data(spokes)
-      .enter()
-      .append('svg:line')
-      .attr('class', function (i) {
-        return 'radarLine' + i + ' radarLine';
-      })
-      .attr('x1', '0')
-      .attr('y1', '0')
-      .attr('x2', function (_, i) {
-        return maximumChartDiameter / 2 * Math.cos(i * distanceBetweenSpokes);
-      })
-      .attr('y2', function (_, i) {
-        return maximumChartDiameter / 2 * Math.sin(i * distanceBetweenSpokes);
-      })
-  };
+      // build spokes
+      spokeContainer
+        .append('svg:g')
+        .attr('class', 'radarSpoke')
+        .selectAll('.spoke')
+        .data(spokes)
+        .enter()
+        .append('svg:line')
+        .attr('class', function (i) {
+          return 'radarLine' + i + ' radarLine';
+        })
+        .attr('x1', '0')
+        .attr('y1', '0')
+        .attr('x2', function (_, i) {
+          return radius * Math.cos(i * distanceBetweenSpokes);
+        })
+        .attr('y2', function (_, i) {
+          return radius * Math.sin(i * distanceBetweenSpokes);
+        })
+    };
 
-  var drawLabels = function () {
-    //  Initialize Label container
-    var labelContainer = initCenteredSvg('radarLabelContainer');
+    var drawLabels = function () {
+      //  Initialize Label container
+      var labelContainer = initCenteredSvg('radarLabelContainer');
 
-    // draw the labels
-    labelContainer
-      .append("svg:g")
-      .selectAll(".radarLabel")
-      .data(data.labels)
-      .enter()
+      // draw the labels
+      labelContainer
+        .append("svg:g")
+        .selectAll(".radarLabel")
+        .data(settings.data.labels)
+        .enter()
         .append("svg:text")
         .attr('class', 'radarLabel')
         .attr('x', function (_, i) {
-          return (maximumTextDiameter / 2 * Math.cos(i * distanceBetweenSpokes))
+          return (textRadius * Math.cos(i * distanceBetweenSpokes));
         })
         .attr('y', function (_, i) {
-          return (maximumTextDiameter / 2 * Math.sin(i * distanceBetweenSpokes));
+          return (textRadius * Math.sin(i * distanceBetweenSpokes));
         })
         // position of text relative to spokes
         .attr("text-anchor", function (_, i) {
@@ -143,17 +154,17 @@ var flipogram = function (data) {
           }
         })
         .text(function (label) { return label });
-  };
+    };
 
-  var drawOverlays = function () {
-    // init overlay container
-    var overlaysContainer = initCenteredSvg('radarOverlayContainer');
+    var drawOverlays = function () {
+      // init overlay container
+      var overlaysContainer = initCenteredSvg('radarOverlayContainer');
 
-    // Initialize loop for each individual overlay layer
-    overlaysContainer
-      .selectAll(".overlay")
-      .data(data.values)
-      .enter()
+      // Initialize loop for each individual overlay layer
+      overlaysContainer
+        .selectAll(".overlay")
+        .data(settings.data.values)
+        .enter()
         .append("svg:polygon")
         .attr('class', function(_, i) {
           return 'radarOverlay' + i + ' radarOverlay';
@@ -168,29 +179,18 @@ var flipogram = function (data) {
           });
           return points
         })
+        .attr('stroke', function (_, i) { return settings.colors[i] })
+        .attr('fill',   function (_, i) { return settings.colors[i] })
+    };
 
-        // could this be done with css?
-        .attr('stroke', function (_, i) { return colors[i] })
-        .attr('fill',   function (_, i) { return colors[i] })
+    function render() {
+      drawWebbing();
+      drawSpokes();
+      drawLabels();
+      drawOverlays();
+    }
+
+    render();
   };
 
-  function render() {
-    drawWebbing();
-    drawSpokes();
-    drawLabels();
-    drawOverlays();
-  }
-
-  render();
-
-  //redraw stuff should probably be done outside of this so that we keep this module pure (eg not querying the dom except for graphing)
-
-  // function redraw() {
-  //   $('#radarSvg').remove();
-  //   render();
-  // }
-  //
-  // return exposedFunctions = {
-  //   'redraw': redraw
-  // }
-};
+}( jQuery ));
